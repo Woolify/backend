f1ed3398902353377a9db98a0c197bdfe3a0559e
Deep Bansode <drasticoder@gmail.com>
2023-09-11 19:09:44 +0530

    add(db): animals schema

---
+++ models/Animal.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "farmer",
+      required: true,
+    },
+    nickName: {
+      type: String,    
+    },
+    slug: {
+      type: String,    
+    },
+    qrImg:{
+      type:String,
+    },
+    age: {
+      type: String,    
+    },
+    weight:{
+      type:String,
+    },
+    type:{
+      type: String,
+      enum: [ "sheep","goat","yak","rabbit"],
+      default:"sheep",
+    },
+    breed:{ 
+      type: String,
+      enum: ["nali","deccani","bannur","malpura","gaddi","garole","muzzaffarnagari","mecheri","jaisalmeri","chokla"],
+    },    
+    descp: {
+      type: String,
+    },
+    saveAsDraft: {
+      type: Boolean,
+      default: true,
+    },    
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Sheep = mongoose.model("sheep", schema ) 
