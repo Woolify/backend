8683f29eb84aa44cf828402b0c61906c558acaad
Deep Bansode <drasticoder@gmail.com>
2023-09-11 19:10:39 +0530

    chore(auth): update routes

--- routes/user/authRoute.js
+++ routes/user/authRoute.js
@@ -1,38 +1,32 @@
 import express from "express";
 import {
+  loginUser,
   registerUser,
-  // logoutUser ,
-  // userForgotPassword,
+  userForgotPassword,
   userResetPassword,
   userChangePassword,
 } from "../../controllers/user/authController.js";
-import passport from "passport";
 
 const router = express.Router();
 
-router.route("/register").post(registerUser)
-
 router
   .route("/login")
-  .post(
-    passport.authenticate("userLocal", {
-      failureRedirect: "/api/user/auth/login",
-      failureFlash: true,
-      successFlash: `Welcome To Web Admin Dashboard`,
-    }),
-    (req, res) => {
-      res.redirect("/api/user/");
-    }
-  )
+  .post(loginUser);
 
-// router
-//   .route("/forget-password")
-//   .post(userForgotPassword)
-//
-// router
-//   .route("/reset-password/:token")
-//   .put(userResetPassword)
-//
-// router.route("/change-password").put(userChangePassword);
+router
+  .route("/register")
+  .post(registerUser)
+
+router
+  .route("/forget-password")
+  .post(userForgotPassword)
+
+router
+  .route("/reset-password/:token")
+  .put(userResetPassword)
+
+router
+  .route("/change-password")
+  .put(userChangePassword);
 
 export default router;
