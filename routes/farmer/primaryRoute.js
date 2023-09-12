411c57a4b21e94bd9d6e8d928fba2618de3c22a9
Deep Bansode <drasticoder@gmail.com>
2023-09-13 00:02:43 +0530

    chore: route for generating& validating qr code

--- routes/farmer/primaryRoute.js
+++ routes/farmer/primaryRoute.js
@@ -2,18 +2,24 @@ import express from "express";
 import {
     generateQr,
     updateAnimalData,
-
+    getSingleAnimalData
 } from "../../controllers/farmer/primaryController.js";
+import upload from '../../middleWares/uploads.js'
+import { authorizedUser } from "../../middleWares/accessAuth.js";
 
 const router = express.Router();
 
 router
   .route("/generate-qr")
-  .post(generateQr);
+  .post(authorizedUser,upload,generateQr);
 
 router
   .route("/update-animal")
   .post(updateAnimalData);
+
+router
+  .route("/animal/:id")
+  .get(getSingleAnimalData);
 
 
 export default router;
