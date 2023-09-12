7acc6a975858a9af2a096cc2de334119a0a0b1e7
Deep Bansode <drasticoder@gmail.com>
2023-09-12 17:22:19 +0530

    chore(auth): update user extraction func

--- middleWares/accessAuth.js
+++ middleWares/accessAuth.js
@@ -1,98 +1,52 @@
-import passport from "passport";
-import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
-import { Strategy as localStrategy } from "passport-local";
-import { Creator } from "../models/Creator.js";
-import { Learner } from "../models/Learner.js";
-import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+import { User } from "../models/User.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
 
-config({ path: "./config/config.env" });
-
-// Local strategies for creator and admin
-passport.use(
-  "creatorLocal",
-  new localStrategy(async function (username, password, next) {
-    try {
-      const creator = await Creator.findOne({ email: username }).select(
-        "+password"
-      );
-      if (!creator)
-        return next(null, false, { message: "Email is not registered" });
-
-      if (!creator.status)
-        return next(null, false, {
-          message: "This account is deactivated by admin.",
-        });
-
-      let isMatch = await creator.comparePassword(password);
-      if (isMatch) {
-        return next(null, creator);
-      } else {
-        return next(null, false, {
-          message: "Incorrect credentials, Please try again.",
-        });
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  })
-);
-
-// JWT strategies for learners
-let opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.JWT_SECRET;
-
-passport.use(
-  new JWTStrategy(opts, async (token, done) => {
-    try {
-      const learner = await Learner.findById(token.id);
-      if (learner && !learner.status) {
-        return done(null, false, { message: "Account has been deactivated" });
-      }
-      return done(null, token.id);
-    } catch (error) {
-      console.log(error);
-      done(error);
-    }
-  })
-);
-
-//Serializing & DeSerializing for browser session
-passport.serializeUser(function (user, next) {
-  let key = {
-    _id: user._id,
-    type: user.type,
-  };
-  next(null, key);
-});
-
-passport.deserializeUser(async function (key, next) {
-  let Model;
-  switch (key.type) {
-    case "admin":
-      Model = Admin;
-      break;
-    case "creator":
-      Model = Creator;
-      break;
-    case "learner":
-      Model = Learner;
-      break;
-    default:
-      break;
+export const authorizedUser = (req, res, next) => {
+  if (req.user && req.user.type === "user") {
+    return next();
   }
-  try {
-    let user;
-    if (key.type === "creator") {
-      user = await Model.findOne({ _id: key._id }).populate({
-        path: "createdBy",
-        select: "brandName",
-      });
+  req.flash("error", "Please authenticate first!");
+  return res.redirect("/api/user/auth/login");
+};
+
+export const checkUserModuleAccess = (moduleName) => {
+  return (req, res, next) => {
+    if (req.user.role === "user" || req.user.modules.includes(moduleName)) {
+      next();
     } else {
-      user = await Model.findOne({ _id: key._id });
+      req.flash("error", "Don't have access to this module!");
+      return res.redirect("/api/user/auth/login");
     }
-    next(null, user);
-  } catch (error) {
-    next(error, false);
+  };
+};
+export const extractUserInfo = async (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    return next();
   }
-});
+
+  try {
+    const jwtToken = authHeader.match(/^Bearer (.+)/)[1];
+    const decodedHeader = jwt.decode(jwtToken, { complete: true });
+
+    if (!decodedHeader || !decodedHeader.payload.id) {
+      return next();
+    }
+
+    const user = await User.findById(decodedHeader.payload.id);
+
+    if (user) {
+      req.user = user;
+      res.status(200).json({user : req.user})
+    } else {
+      console.error('User not found');
+      next();
+    }
+  } catch (error) {
+    console.error('Error extracting user info:', error);
+    next();
+  }
+
+};
