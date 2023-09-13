7c124ad4c62284863379b0e4d4f6a4effefc37e4
Deep Bansode <drasticoder@gmail.com>
2023-09-13 20:23:16 +0530

    chore: user crud (WIP)

---
+++ controllers/user/primaryController.js
@@ -0,0 +1,13 @@
+import { catchAsyncError } from "../../middleWares/catchAsyncError.js";
+
+export const getUser = catchAsyncError(async(req,res,next) => {
+    
+})
+
+export const updateUser = catchAsyncError(async(req,res,next) => {
+
+})
+
+export const deleteUser = catchAsyncError(async(req,res,next) => {
+
+})
