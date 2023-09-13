3b6eaa82ff52cfbf2d8212fbb9a99ea2c01dbb5c
Deep Bansode <drasticoder@gmail.com>
2023-09-13 23:03:05 +0530

    feat: user read& delete func

--- controllers/user/primaryController.js
+++ controllers/user/primaryController.js
@@ -1,7 +1,16 @@
+import {User} from "../../models/User.js"
 import { catchAsyncError } from "../../middleWares/catchAsyncError.js";
 
 export const getUser = catchAsyncError(async(req,res,next) => {
-    
+    const {id} = req.params;
+  
+    const user = await User.findById(id);
+  
+    if (!user) {
+      return res.status(404).json({message:"user not found!"});
+    }else {
+      return res.status(200).json({user})
+    }
 })
 
 export const updateUser = catchAsyncError(async(req,res,next) => {
@@ -9,5 +18,6 @@ export const updateUser = catchAsyncError(async(req,res,next) => {
 })
 
 export const deleteUser = catchAsyncError(async(req,res,next) => {
-
+    await User.findByIdAndUpdate(req.params.id, {deleted:true});
+    res.status(200).json({message: "User deleted successfully."})
 })
