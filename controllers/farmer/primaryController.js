f5ea06262da6b086da88ef7a24273b3153645af8
Deep Bansode <drasticoder@gmail.com>
2023-09-13 22:36:59 +0530

    feat: generate qr & pdf generation func

--- controllers/farmer/primaryController.js
+++ controllers/farmer/primaryController.js
@@ -1,9 +1,9 @@
-import {v4 as uuidv4} from "uuid";
 import qrcode from "qrcode"
 import fs from 'fs'
 import { catchAsyncError } from "../../middleWares/catchAsyncError.js"
 import {Animal} from "../../models/Animal.js";
 import {Farmer} from "../../models/Farmer.js";
+import PDFDocument from "pdfkit"
 
 
 const addAnimal = async(owner,name) => {
@@ -17,15 +17,16 @@ const addAnimal = async(owner,name) => {
     const qrDataUrl = await qrcode.toDataURL(data);
     const qrImageBuffer = Buffer.from(qrDataUrl.replace(/^data:image\/\w+;base64,/, ''), 'base64');
   
-    let filePath = `./public/uploads/animals/qr/qrImg-${ Date.now() + Math.floor(Math.random() * 90000) }.jpg`;
-    fs.writeFileSync( filePath, qrImageBuffer);
+    // let filePath = `./public/uploads/animals/qr/qrImg-${ Date.now() + Math.floor(Math.random() * 90000) }.jpg`;
+    // fs.writeFileSync( filePath, qrImageBuffer);
 
-    _animal.qrImg = process.env.BACKEND_URL + filePath.substring(8);
-    _animal.nickName = name
+    // _animal.qrImg = process.env.BACKEND_URL + filePath.substring(8);
+    // _animal.nickName = name
 
-    await _animal.save();  
-    return process.env.BACKEND_URL +filePath.substring(8);
-}
+    // await _animal.save();  
+    // return filePath;
+  return qrImageBuffer;
+  }
 
 export const generateQr = catchAsyncError(async (req,res,next) =>{
     const {count} = req.body;
@@ -34,7 +35,43 @@ export const generateQr = catchAsyncError(async (req,res,next) =>{
   for (let i = 1; i <= count; i++) {
     result.push(await addAnimal(req.user._id,`SH${i}`));
   }
-  return res.status(200).json(result)
+  // return res.status(200).json(result)
+
+  let filePath = `./public/uploads/animals/pdf/qrPdf-${ Date.now() + Math.floor(Math.random() * 90000) }.pdf`;
+
+  const doc = new PDFDocument({ autoFirstPage: false });
+  const stream = fs.createWriteStream(filePath);
+  
+  doc.pipe(stream);
+
+  for (const imagePath of result) {
+    doc.addPage({ size: [612, 612] }); 
+
+    try {
+      await new Promise((resolve, reject) => {
+        try {
+          doc.image(imagePath, 0, 0, { width: 612, height: 612 });
+          resolve();
+        } catch (error) {
+          reject(error);
+        }
+      });
+    } catch (error) {
+      console.error(`Error embedding image: ${error}`);
+    }
+  }
+
+  doc.end();
+
+  let outputUrl = process.env.BACKEND_URL + filePath.substring(8);
+
+  stream.on('finish',() => {
+    res.status(200).json({"pdfUrl": outputUrl });
+  })
+  stream.on('error',() => {
+    res.status(500).json({message: "Unable to generate pdf!"});
+  })
+
 })
 export const updateAnimalData = catchAsyncError(async (req,res,next) =>{
   const {
