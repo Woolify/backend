d68a5e9c2e20f9aa077ffe31e1c9f81c9128d996
Deep Bansode <drasticoder@gmail.com>
2023-09-15 22:11:58 +0530

    feat: add bid func

--- routes/bid/bidRoute.js
+++ routes/bid/bidRoute.js
@@ -5,6 +5,7 @@ import {
   createBid,
   updateBid,
   deleteBid,
+  addBid,
 } from "../../controllers/bid/bidController.js";
 import { authorizedUser } from "../../middleWares/accessAuth.js";
 
@@ -17,11 +18,16 @@ router
 router
   .route("/list")
   .get(authorizedUser,getAllBids)
+
+router
+  .route("/add/:id")
+  .put(authorizedUser,addBid);
   
   router
     .route("/:id")
     .get(authorizedUser,getSingleBid)
     .put(authorizedUser,updateBid)
     .delete(authorizedUser,deleteBid);
+
 
 export default router;
