f651cea29de0efdd27ac23edb90ef0f99339228f
Deep Bansode <drasticoder@gmail.com>
2023-09-19 13:16:59 +0530

    add(db) : auction schema

---
+++ models/Auction.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const schema = new mongoose.Schema(
+    {
+        initializer: {
+          type: Schema.Types.ObjectId,
+          ref: "farmer",
+          required: true,
+        },
+        basePrice: {
+          type: Number,
+          required: true,
+        },
+        maxPrice:{
+          type: Number,
+          validate: {
+            validator: function (value) {
+              return value >= this.basePrice;
+            },
+            message: "max price cannot be less than base Price",
+          },
+        },
+        quantity:{
+            type:Number,
+            required:true,
+        },
+        inventory:{
+            type: Schema.Types.ObjectId,
+            ref:"inventory",
+            required:true,
+        },
+        status:{
+            type: Boolean,
+            default : false
+        },
+        lockedBy:{
+          type: Schema.Types.ObjectId,
+          ref:"ventor",
+        },
+        descp:{
+          type: String,
+        },
+        bids: [
+          {
+            type: Schema.Types.ObjectId,
+            ref: "bid",
+            required: true,
+          },
+        ],
+      },
+      { timestamps: true }
+    );
+
+export const Auction = mongoose.model("auction", schema )
