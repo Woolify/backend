18f69f6685ea1f67e43fd35286d7b8ff16d9f299
Deep Bansode <drasticoder@gmail.com>
2023-09-24 02:24:36 +0530

    chore(db) :  add notification schema

---
+++ models/Notification.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  userId: {
+    type:String,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  read: {
+    type: Boolean,
+    default: false,
+  },
+},
+{ timestamps: true }
+);
+
+export const Notification = mongoose.model("notification", schema )
