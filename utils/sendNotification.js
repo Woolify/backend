b606537303d71af53b722a74d61ba4e97af34637
Deep Bansode <drasticoder@gmail.com>
2023-09-24 02:23:28 +0530

    feat : send notification util

---
+++ utils/sendNotification.js
@@ -0,0 +1,49 @@
+import {Notification} from "../models/Notification.js";
+
+export const createNotification = async (userId, message) => {
+  try {
+    const notification = new Notification({
+      userId,
+      message,
+    });
+
+    await notification.save();
+  } catch (error) {
+    console.error("Error creating notification:", error);
+  }
+};
+
+export const sendNotification = async (io, userId,socketId, content) => {
+  try {
+    const notification = await Notification.create({ userId, content });
+
+    const userSocket = io.sockets.sockets.get(socketId);
+    if (userSocket) {
+      userSocket.emit("notification", notification);
+    }
+  } catch (error) {
+    console.error("Error sending notification:", error);
+  }
+};
+
+
+export const getNotificationsForUser = async (userId) => {
+  try {
+    const notifications = await Notification.find({ userId }).sort({
+      createdAt: -1,
+    });
+
+    return notifications;
+  } catch (error) {
+    console.error("Error fetching notifications:", error);
+    return [];
+  }
+};
+
+export const markNotificationAsRead = async (notificationId) => {
+  try {
+    await Notification.findByIdAndUpdate(notificationId, { read: true });
+  } catch (error) {
+    console.error("Error marking notification as read:", error);
+  }
+};
